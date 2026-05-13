d50917c492ec6e6068368557ed49cc99fa1be753
Andrii Karnaukh <a.karnaukh@sagax.software>
2026-05-13 09:43:18 +0300

    refactor: add setnry

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as Sentry from "@sentry/react";
 import { Toaster } from 'sonner';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
@@ -20,6 +21,13 @@ const queryClient = new QueryClient({
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').catch(console.error);
 }
+
+Sentry.init({
+  dsn: "https://ca810ea2f498b04ffe61716d69e61155@o4511381200502784.ingest.de.sentry.io/4511381205221456",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
